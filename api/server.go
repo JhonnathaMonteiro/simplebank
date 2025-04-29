aa8331b6fe933a94eed5cbd6830f5c07cdd68bbb
Jhonnatha Monteiro <jhonnatha.am@gmail.com>
2025-04-29 02:18:38 -0300

    fe

--- api/server.go
+++ api/server.go
@@ -19,6 +19,7 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
 
+	// T
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
